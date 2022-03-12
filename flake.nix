e7c2e85e7eb0d66e0d8b0bbe5e02df201b0fa318
hugosenari <hugosenari@gmail.com>
2022-03-12 01:16:09 -0300

    chore: re index cmake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''CMake for Nimble'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."cmake-master".url = "path:./master";
+  inputs."cmake-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cmake-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."cmake-v0_1_1".url = "path:./v0_1_1";
+  inputs."cmake-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cmake-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''CMake for Nimble'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-cmake-master.flake = false;
+  inputs.src-cmake-master.owner = "genotrance";
+  inputs.src-cmake-master.ref   = "refs/heads/master";
+  inputs.src-cmake-master.repo  = "cmake";
+  inputs.src-cmake-master.type  = "github";
+  
+  inputs."nimterop".dir   = "nimpkgs/n/nimterop";
+  inputs."nimterop".owner = "riinr";
+  inputs."nimterop".ref   = "flake-pinning";
+  inputs."nimterop".repo  = "flake-nimble";
+  inputs."nimterop".type  = "github";
+  inputs."nimterop".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimterop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-cmake-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-cmake-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
